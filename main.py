a533fdb9a7a6de12b249b6b279944e6113f533df
vanmanhpro <vanmanhpbc1998@gmail.com>
2019-01-29 23:39:53 +0700

    update Jenkins 3

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    keycloak_admin = KeycloakAdmin(server_url='https://ec2-13-54-104-82.ap-southeast-2.compute.amazonaws.com:8443/auth/',
+    keycloak_admin = KeycloakAdmin(server_url='https://ec2-13-54-104-82.ap-southeast-2.compute.amazonaws.com/auth/',
                                     username=sys.argv[1],
                                     password=sys.argv[2],
                                     realm_name='e2e-tenancy',
