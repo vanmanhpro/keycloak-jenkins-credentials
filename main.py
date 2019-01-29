c7e875dea083a052365b6d054bba00c283cb6740
vanmanhpro <vanmanhpbc1998@gmail.com>
2019-01-29 12:51:17 +0700

    for using credentials

---
+++ main.py
@@ -0,0 +1,14 @@
+from keycloak import KeycloakAdmin
+import json
+import sys
+
+
+
+if __name__ == "__main__":
+    keycloak_admin = KeycloakAdmin(server_url='https://ec2-13-54-104-82.ap-southeast-2.compute.amazonaws.com:8443/auth/',
+                                    username=sys.argv[1],
+                                    password=sys.argv[2],
+                                    realm_name='e2e-tenancy',
+                                    verify=False)
+                                    
+    print(json.dumps(keycloak_admin.get_clients(), indent=2))
